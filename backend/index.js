7b4ecae3243093563903d1268a0a116b8a1efd7c
ltphat2204 <ltphat2204@gmail.com>
2024-06-16 18:49:19 +0700

    Add upload file api

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,18 @@ dotenv.config();
 dotenv.config({ path: `.env.local`, override: true });
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+    const filePath = req.file.path;
+    res.json(filePath);
+});
 
 const PORT = process.env.PORT || 3000;
 
