4cf3a924efe706d13e432193595af4db2db2f146
ltphat2204 <ltphat2204@gmail.com>
2024-06-16 16:51:50 +0700

    fix bug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
+import './styles/index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Signin from './pages/Signin.jsx';
 import EditProfile from './pages/EditProfile.jsx';
