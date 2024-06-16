59225089f5f549f4d6b9bc9dc3a9ac2664436a8d
ltphat2204 <ltphat2204@gmail.com>
2024-06-16 17:22:19 +0700

    setup server

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config();
+dotenv.config({ path: `.env.local`, override: true });
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
